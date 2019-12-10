d07b54d7fe74c8c6deae2a966acc5e44030136d2
oliverjam <oliverjamesphillips@gmail.com>
2019-12-10 17:01:52 +0000

    Test for promise rejection as well

--- workshop/request.test.js
+++ workshop/request.test.js
@@ -1,19 +1,37 @@
-const test = require("tape");
+const tape = require("tape");
 const nock = require("nock");
+var _test = require("tape-promise").default;
+var test = _test(tape); // add promise support to tape
+
 const request = require("./request");
 
 test("request fetches data correctly", t => {
-  nock("http://jsonplaceholder.typicode.com")
+  nock("https://jsonplaceholder.typicode.com")
     .get("/users/1")
     .reply(200, {
       name: "Leanne Graham",
     });
-  request("http://jsonplaceholder.typicode.com/users/1").then(response => {
-    t.deepEqual(
-      response.name,
-      "Leanne Graham",
-      "the response body should contain the correct json"
-    );
-    t.end();
-  });
+
+  return request("https://jsonplaceholder.typicode.com/users/1", () => {}).then(
+    response => {
+      t.deepEqual(
+        response.name,
+        "Leanne Graham",
+        "the response should contain the correct json"
+      );
+      t.end();
+    }
+  );
+});
+
+test("request rejects with http error correctly", t => {
+  nock("https://jsonplaceholder.typicode.com")
+    .get("/users/1")
+    .reply(500, { error: "Oops" });
+
+  return t.rejects(
+    request("https://jsonplaceholder.typicode.com/users/1", () => {}),
+    new Error(500),
+    "Should reject with 500 error"
+  );
 });
