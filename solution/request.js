80ed87888b5130582684a4b8ff5bdb10b601f764
oliverjam <oliverjamesphillips@gmail.com>
2019-12-10 17:01:32 +0000

    Move request options out of function for readability

--- solution/request.js
+++ solution/request.js
@@ -1,30 +1,29 @@
 const https = require("https");
 
+// Github's API doesn't work without a user-agent header :(
+const options = { headers: { "User-Agent": "GitHub requires this" } };
+
 /**
  * TASK 1
  */
 function request(url) {
   return new Promise((resolve, reject) => {
     https
-      .get(
-        url,
-        { headers: { "User-Agent": "GitHub requires this" } },
-        response => {
-          let data = "";
-          response.on("data", chunk => {
-            data += chunk;
-          });
-          response.on("end", () => {
-            const statusCode = response.statusCode;
-            if (statusCode >= 400) {
-              reject(new Error(statusCode));
-              return; // stop executing after error
-            }
-            const body = JSON.parse(data);
-            resolve(body);
-          });
-        }
-      )
+      .get(url, options, response => {
+        let data = "";
+        response.on("data", chunk => {
+          data += chunk;
+        });
+        response.on("end", () => {
+          const statusCode = response.statusCode;
+          if (statusCode >= 400) {
+            reject(new Error(statusCode));
+            return; // stop executing after error
+          }
+          const body = JSON.parse(data);
+          resolve(body);
+        });
+      })
       .on("error", err => reject(err));
   });
 }
--- workshop/request.js
+++ workshop/request.js
@@ -1,26 +1,25 @@
 const https = require("https");
 
+// Github's API doesn't work without a user-agent header :(
+const options = { headers: { "User-Agent": "GitHub requires this" } };
+
 function request(url, cb) {
   https
-    .get(
-      url,
-      { headers: { "User-Agent": "GitHub requires this" } },
-      response => {
-        let data = "";
-        response.on("data", chunk => {
-          data += chunk;
-        });
-        response.on("end", () => {
-          const statusCode = response.statusCode;
-          if (statusCode >= 400) {
-            cb(new Error(statusCode));
-            return; // stop executing after error
-          }
-          const body = JSON.parse(data);
-          cb(null, body);
-        });
-      }
-    )
+    .get(url, options, response => {
+      let data = "";
+      response.on("data", chunk => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        const statusCode = response.statusCode;
+        if (statusCode >= 400) {
+          cb(new Error(statusCode));
+          return; // stop executing after error
+        }
+        const body = JSON.parse(data);
+        cb(null, body);
+      });
+    })
     .on("error", err => cb(err));
 }
 
